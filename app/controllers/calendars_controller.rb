1b0f9fe5ef36beebf9eed24c982809c8484c8a29
winwinaungsein <seins1283@gmail.com>
2026-09-03 20:57:38 +0900

    fix calendar issues

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -40,8 +39,9 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { :month  (@todays_date + x).month, date:  (@todays_date + x).day, plans:  today_plans, wday:  wdays[wday_num] }
 
-       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
-      @week_days.push(days)
+       @week_days.push(days)
     end
+  end
+end
